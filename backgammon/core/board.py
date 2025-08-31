5d820bbc35ae9f4fc3fb0ab4b60c87c21c46e26a
LucasDanielClavero <l.clavero@alumno.um.edu.ar>
2025-08-30 22:34:53 -0300

    nueva funcion agregada a board y changelog actualizado

--- backgammon/core/board.py
+++ backgammon/core/board.py
@@ -7,3 +7,17 @@ class Board:
 
     def __repr__(self) -> str:
         return f"Board(points={len(self._points_)})"
+    
+    def _setup_board_(self):
+        self._points_ = [[] for _ in range(24)]
+        self._bar_ = {"white": [], "black": []}
+
+        self._points_[0].extend([Checker("white")] * 2)
+        self._points_[11].extend([Checker("white")] * 5)
+        self._points_[16].extend([Checker("white")] * 3)
+        self._points_[18].extend([Checker("white")] * 5)
+
+        self._points_[23].extend([Checker("black")] * 2)
+        self._points_[12].extend([Checker("black")] * 5)
+        self._points_[7].extend([Checker("black")] * 3)
+        self._points_[5].extend([Checker("black")] * 5)
--- backgammon/documentation/CHANGELOG.md
+++ backgammon/documentation/CHANGELOG.md
@@ -1,9 +1,14 @@
 # Changelog
 Todas las versiones notables de este proyecto se documentarán en este archivo.
 
+## [1.2.0] - 2025-08-28
+### Agregado
+- clases player y board
+
 ## [1.1.0] - 2025-08-28
 ### Agregado
 - clases checker y dice
+
 ## [1.0.0] - 2025-08-26
 ### Agregado
 - Versión inicial del proyecto.
