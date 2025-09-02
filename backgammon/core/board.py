02a775dd51c36f1956023f5653febe940889a8e0
LucasDanielClavero <l.clavero@alumno.um.edu.ar>
2025-09-01 23:17:15 -0300

    agrego funcion que verifica si el movimiento es valido

--- backgammon/core/board.py
+++ backgammon/core/board.py
@@ -21,3 +21,23 @@ class Board:
         self._points_[12].extend([Checker("black")] * 5)
         self._points_[7].extend([Checker("black")] * 3)
         self._points_[5].extend([Checker("black")] * 5)
+
+    def is_valid_move(self, start_point: int, end_point: int, player_color: str) -> bool:
+        start_idx = start_point - 1
+        end_idx = end_point - 1
+
+        if not (0 <= start_idx < 24 and 0 <= end_idx < 24):
+            return False
+
+        if not self._points_[start_idx] or self._points_[start_idx][0]._color_ != player_color:
+            return False
+
+        destination_checkers = self._points_[end_idx]
+        
+        if destination_checkers and destination_checkers[0]._color_ != player_color and len(destination_checkers) > 1:
+            return False
+
+        if player_color == "white" and start_idx < end_idx:
+            return False
+        if player_color == "black" and start_idx > end_idx:
+            return False
